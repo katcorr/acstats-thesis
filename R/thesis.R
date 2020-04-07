0bbc0dd352c3ab8b61fe0e924f19753c5e23cf6e
Brittney E. Bailey <bebailey@amherst.edu>
2020-04-07 18:22:35 -0400

    Added guide to file structure

--- R/thesis.R
+++ R/thesis.R
@@ -1,7 +1,7 @@
 #' Creates an R Markdown PDF Thesis document
 #'
 #' This is a function called in output in the YAML of the driver Rmd file
-#' to specify using the Reed College Senior Thesis LaTeX template and cls files.
+#' to specify using the Amherst College Senior Thesis LaTeX template and cls files.
 #'
 #' @export
 #' @param toc A Boolean (TRUE or FALSE) specifying whether table of contents should be created
